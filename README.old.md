d781044bc8d121d4af0a3eb4dfeb9a97b12c7237
Dustin Lennon <dustin.lennon@stelth2000inc.com>
2024-05-31 10:09:26 -0400

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# reactjs-interview-qr-code-generator-project
+A QR Code Generator built in ReactJS
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QrCodeGenerator from './components/qr-code-generator';
+
+function App() {
+  return (
+    <div className="App">
+      <QrCodeGenerator />
+    </div>
+  );
+}
+
+export default App;
